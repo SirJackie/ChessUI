31c12a33efa9537c10e3b27a2be81074392d745c
lyj00912 <3122742760@qq.com>
2023-03-19 12:45:44 +0800

    optimized the code

--- WuZiQi.py
+++ WuZiQi.py
@@ -2,7 +2,13 @@ import tkinter as tk
 from NativeAPI import EnableHighDPISupport, GetScreenResolution
 
 
+# Preferences
 highDPI = True
+screenResolution = GetScreenResolution()
+if highDPI:
+    halfGridSize = int(0.05 * screenResolution[1])
+else:
+    halfGridSize = 15
 
 
 def CreateWindow(width, height, title):
@@ -37,47 +43,40 @@ def CreatePiece(canvas, i, j, halfGridSize, color):
     CreateCircle(canvas, x * 2 * halfGridSize, y * 2 * halfGridSize, int(0.85 * halfGridSize * 2), color)
 
 
-width = 9
-height = 9
-screenResolution = GetScreenResolution()
-if highDPI:
-    halfGridSize = int(0.05 * screenResolution[1])
-else:
-    halfGridSize = 15
-winWidth = width * 2 * halfGridSize
-winHeight = height * 2 * halfGridSize
-
-root = CreateWindow(winWidth, winHeight, "ChessUI")
-canvas = CreateCanvas(root, 0, 0, winWidth, winHeight)
-
-# Horizontal
-for dy in range(0, height):
-    x1 = halfGridSize
-    y1 = halfGridSize + dy * 2 * halfGridSize
-    x2 = halfGridSize + (width - 1) * 2 * halfGridSize
-    y2 = halfGridSize + dy * 2 * halfGridSize
-    canvas.create_line(x1, y1, x2, y2)
-
-# Vertical
-for dx in range(0, width):
-    x1 = halfGridSize + dx * 2 * halfGridSize
-    y1 = halfGridSize
-    x2 = halfGridSize + dx * 2 * halfGridSize
-    y2 = halfGridSize + (height - 1) * 2 * halfGridSize
-    canvas.create_line(x1, y1, x2, y2)
-
-
-def play(event):
+def MouseClickCallback(event):
     mx, my = event.x, event.y
 
     i = my // (2 * halfGridSize)
     j = mx // (2 * halfGridSize)
     print(i, j)
-    
+
     CreatePiece(canvas, i, j, halfGridSize, color="black")
 
 
-canvas.bind("<Button-1>", play)
+if __name__ == "__main__":
+    width = 9
+    height = 9
+    winWidth = width * 2 * halfGridSize
+    winHeight = height * 2 * halfGridSize
 
+    root = CreateWindow(winWidth, winHeight, "ChessUI")
+    canvas = CreateCanvas(root, 0, 0, winWidth, winHeight)
 
-root.mainloop()
+    # Horizontal
+    for dy in range(0, height):
+        x1 = halfGridSize
+        y1 = halfGridSize + dy * 2 * halfGridSize
+        x2 = halfGridSize + (width - 1) * 2 * halfGridSize
+        y2 = halfGridSize + dy * 2 * halfGridSize
+        canvas.create_line(x1, y1, x2, y2)
+
+    # Vertical
+    for dx in range(0, width):
+        x1 = halfGridSize + dx * 2 * halfGridSize
+        y1 = halfGridSize
+        x2 = halfGridSize + dx * 2 * halfGridSize
+        y2 = halfGridSize + (height - 1) * 2 * halfGridSize
+        canvas.create_line(x1, y1, x2, y2)
+
+    canvas.bind("<Button-1>", MouseClickCallback)
+    root.mainloop()
