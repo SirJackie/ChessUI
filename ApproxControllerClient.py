f5813cfb7223f6d581823afe810ae876c903e919
lyj00912 <3122742760@qq.com>
2023-03-19 13:36:46 +0800

    created Bridge Server, and 2 Approx Clients, removed HiRes.py

---
+++ ApproxControllerClient.py
@@ -0,0 +1,15 @@
+import json
+from JSock import JSock
+
+jsock = JSock()
+jsock.Connect("127.0.0.1", 16520)
+
+# jsock.SendStr("CloseServer")
+
+# jsock.SendStr("SetState")
+# jsock.SendStr(json.dumps(
+#     [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+# ))
+
+jsock.SendStr("GetAction")
+print(jsock.RecvStr())
